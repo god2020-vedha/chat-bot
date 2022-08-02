02619cd0efa2dfd4adc1660f2b95eafcb424f0d1
god2020-vedha <diana606344@gmail.com>
2022-08-03 00:29:27 +0530

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # Importing necessary libraries
 import numpy as np
 from flask import Flask, request, make_response
+from multiprocessing import Process
 import json
 import pickle
 from flask_cors import cross_origin
@@ -9,27 +10,26 @@ from flask_cors import cross_origin
 app = Flask(__name__)
 
 #Loading the model from pickle file
-model = pickle.load(open('rf.pkl', 'rb'))
-
-
+model = pickle.load(open('rf1.pkl','rb'))
+@app.route('/')
+@app.route('/home')
+def home():
+    return "WELCOME DIANA"
 # geting and sending response to dialogflow
 @app.route('/webhook', methods=['POST'])
-@cross_origin()
+#@cross_origin()
 def webhook():
-
-
     req = request.get_json(silent=True, force=True)
     res = processRequest(req)
     res = json.dumps(res, indent=4)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
+    #req = request.get_json(force=True)
+    print(r)
     return r
 # processing the request from dialogflow
 def processRequest(req):
-
-
     result = req.get("queryResult")
-    
     #Fetching the data points
     parameters = result.get("parameters")
     Petal_length=parameters.get("number")
@@ -37,7 +37,6 @@ def processRequest(req):
     Sepal_length=parameters.get("number2")
     Sepal_width=parameters.get("number3")
     int_features = [Petal_length,Petal_width,Sepal_length,Sepal_width]
-    
     #Dumping the data into an array
     final_features = [np.array(int_features)]
     
@@ -47,26 +46,26 @@ def processRequest(req):
     #Fitting out model with the data points
     if (intent=='IrisData'):
         prediction = model.predict(final_features)
-    
-        output = round(prediction[0], 2)
-    
-    	
-        if(output==0):
+        print(prediction)
+        output=prediction
+        #output = round(prediction[0], 2)
+       	
+        if(output=='Iris-setosa'):
             flowr = 'Setosa'
     
-        if(output==1):
+        if(output=="Iris-versicolor"):
             flowr = 'Versicolour'
         
-        if(output==2):
+        if(output=="Iris-virginica"):
             flowr = 'Virginica'
             
         #Returning back the fullfilment text back to DialogFlow
         fulfillmentText= "The Iris type seems to be..  {} !".format(flowr)
         #log.write_log(sessionID, "Bot Says: "+fulfillmentText)
-        return {
-            "fulfillmentText": fulfillmentText
-        }
+        return { "fulfillmentText": fulfillmentText}
 
 
 if __name__ == '__main__':
-    app.run()
+    app.secret_key = 'ItIsASecret'
+    app.debug = True
+    app.run(port=5003)
